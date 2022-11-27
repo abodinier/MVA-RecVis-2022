4e484a75695c1634f9f139fd79f14f38678f451a
Alexandre Bodinier <alexandrebodinier@icloud.com>
2022-11-27 22:53:21 +0100

    Refactor

--- src/evaluate_ensemble.py
+++ src/evaluate_ensemble.py
@@ -3,39 +3,33 @@ import json
 import torch
 from tqdm import tqdm
 import PIL.Image as Image
+from pathlib import Path
 
 from model import CNN, ResNet, Inception, VGG, ViT
 from data import get_data_transform
+from ensembling import Averaging, Voting, Stacking
 
 nclasses = 20
 
-resnet_params = json.load(
-    open("experiment_2022-11-26-23-55/models/parameters.json", "r")
-)
-resnet = ResNet(**resnet_params)
-resnet.load_state_dict(
-    torch.load("experiment_2022-11-26-23-55/models/ckp_0.pt")["model"]
-)
+def load_from_exp(exp_dir, model_class):
+    params = json.load(
+        open(exp_dir/"models/parameters.json", "r")
+    )
+    model = model_class(**params)
+    model.load_state_dict(
+        torch.load(exp_dir/"models/ckp_0.pt")["model"]
+    )
+    model.eval()
+    return model
 
-vgg_params = json.load(open("experiment_2022-11-26-23-55/models/parameters.json", "r"))
-vgg = ResNet(**vgg_params)
-vgg.load_state_dict(torch.load("experiment_2022-11-26-23-55/models/ckp_0.pt")["model"])
-
-inception_params = json.load(
-    open("experiment_2022-11-26-23-55/models/parameters.json", "r")
-)
-inception = ResNet(**inception_params)
-inception.load_state_dict(
-    torch.load("experiment_2022-11-26-23-55/models/ckp_0.pt")["model"]
-)
-
-bag_of_models = [resnet, vgg, inception]
-
-use_cuda = torch.cuda.is_available()
+resnet = load_from_exp(Path("EXPERIMENTS/resnet_2022-11-27-00-56"), ResNet)
+vgg = load_from_exp(Path("EXPERIMENTS/vgg_2022-11-27-00-55"), VGG)
+inception = load_from_exp(Path("EXPERIMENTS/inception_2022-11-27-00-54"), Inception)
+bag_of_models = Averaging(models=[resnet, vgg, inception], weights=torch.tensor([1, 1, 1]))
 
 
 test_dir = "../data/test_images/mistery_category"
-data_transforms = get_data_transform(image_size=220, data_augmentation=0)
+data_transforms = get_data_transform(image_size=384, data_augmentation=0)
 
 
 def pil_loader(path):
@@ -50,11 +44,7 @@ for f in tqdm(os.listdir(test_dir)):
     if "jpg" in f:
         data = data_transforms(pil_loader(test_dir + "/" + f))
         data = data.view(1, data.size(0), data.size(1), data.size(2))
-        outputs = torch.zeros((len(bag_of_models), nclasses))
-        for i, model in enumerate(bag_of_models):
-            output = model(data)[0]
-            outputs[i] = output
-        output = outputs.mean(dim=0)
+        output = bag_of_models(data)
         pred = output.argmax().item()
         output_file.write("%s,%d\n" % (f[:-4], pred))
 
