a99adbf9cb32eafe919aeef0dad6a4a14621aea0
Alexandre Bodinier <alexandrebodinier@icloud.com>
2022-11-27 01:09:20 +0100

    Formating

--- src/ensembling.py
+++ src/ensembling.py
@@ -8,14 +8,12 @@ class Averaging:
         self.n_models = len(models)
         self.nclasses = models[0].nclasses
         if weights is not None:
-            assert len(weights) == len(
-                models
-            ), f"There are {len(models)} models and {len(weights)} weights. They must match"
+            assert len(weights) == len(models), f"There are {len(models)} models and {len(weights)} weights. They must match"
         self.weights = weights
-
+    
     def __call__(self, x: torch.tensor) -> torch.tensor:
         outputs = torch.zeros((self.n, x.size(0), self.nclasses))
-
+        
         if self.weights is None:
             for i, model in enumerate(self.models):
                 outputs[i] = model(x)
@@ -25,7 +23,7 @@ class Averaging:
                 outputs[i] = w * model(x)
             outputs = outputs.sum(dim=0)
             outputs /= self.weights.sum()
-
+    
     def predict(self, x: torch.tensor) -> torch.tensor:
         outputs = self.__call__(x)
         return outputs.argmax(dim=1)
@@ -36,14 +34,14 @@ class Voting:
         self.models = models
         self.n_models = len(models)
         self.nclasses = models[0].nclasses
-
+    
     def __call__(self, x: torch.tensor) -> torch.tensor:
         outputs = torch.zeros((x.size(0), self.nclasses))
         for i, model in enumerate(self.models):
             for j, p in enumerate(model(x)):
                 outputs[j, p.argmax()] += 1
         return outputs
-
+    
     def predict(self, x):
         outputs = self.__call__(x)
         return outputs.argmax(dim=1)
@@ -52,6 +50,3 @@ class Voting:
 class Stacking:
     pass
 
-
-class Blending:
-    pass
